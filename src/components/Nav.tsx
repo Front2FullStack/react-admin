e359439773f8c2ff448fe027b2e28a30829cce2d
Sushil Parajuli <sushilparajuli2010@gmail.com>
2021-08-16 15:04:09 +0400

    Creating Layout components and fetching user details with useEffect hook
    - Interface for user object and passing mapping it to props and useState

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { User } from '../models/user';
 
-export default function Nav() {
+export default function Nav(props: {user: User | null}) {
     return (
       <div>
         <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
