df19f7eafa81feb91e53e519425d32ac460236d4
Sushil Parajuli <sushilparajuli2010@gmail.com>
2021-08-16 15:15:34 +0400

    logout function implemented

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,26 @@
+import axios from 'axios';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import { User } from '../models/user';
 
 export default function Nav(props: {user: User | null}) {
-    return (
-      <div>
-        <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-          <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="javascript.void()">
-            Promoter Admin
-          </a>
-          <div className="navbar-nav">
-            <div className="nav-item text-nowrap">
-              <a className="nav-link px-3" href="javascript.void()">
-                Sign out
-              </a>
-            </div>
-          </div>
-        </header>
-      </div>
-    );
+  return (
+    <div>
+      <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+        <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="javascript.void()">
+          Promoter Admin
+        </a>
+        <ul className="my-2 my-md-0 mr-md-3">
+          <Link to={'/profile'}  className="p-2 text-white text-decoration-none">
+            {props.user?.first_name} {props.user?.last_name}
+          </Link>
+            <Link  to={'/login'} className="p-2 text-white text-decoration-none" href="javascript.void()"
+            onClick={async () => { await axios.post('logout')}}
+            >
+              Sign out
+            </Link>
+        </ul>
+      </header>
+    </div>
+  );
 }
